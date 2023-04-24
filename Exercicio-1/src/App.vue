87bf4052f8c082f51159d733e2b62b70dbaf467f
GabrielLima2803 <gabriellima2803@gmail.com>
2023-04-24 11:51:59 -0300

    feito

---
+++ Exercicio-1/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+const cidades = ref([    
+    'São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Salvador',
+    'Fortaleza',
+    'Curitiba',
+    'Manaus',
+    'Recife',
+    'Porto Alegre',
+    'Brasília'
+    ]
+    )
+</script>
+
+<template>
+  <div class="display">
+  <div class="container tema">
+    <h1>Exercicio-1</h1>
+    <hr>
+    <ul>
+      <li v-for="(cidade, index) in cidades" :key="index">{{ cidade }}</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<style scoped>
+ .tema{
+    background-color: black;
+    padding: 30px 30px;
+    color: white;
+    border-radius: 15px;
+    width: 30%;
+    text-align: center;
+    margin-top: 15%;
+  }
+</style>
---
+++ Exercicio-2/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+let cidades = ref([    
+    'São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Salvador',
+    'Fortaleza',
+    'Curitiba',
+    'Manaus',
+    'Recife',
+    'Porto Alegre',
+    'Brasília'
+    ]
+    )
+  
+</script>
+
+<template>
+  <div class="display">
+  <div class="container tema">
+    <h1>Exercicio-2</h1>
+    <hr>
+    <ul>
+      <li v-for="(cidade, index) in cidades.sort()" :key="index">{{ cidade }}</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<style scoped>
+ .tema{
+    background-color: black;
+    padding: 30px 30px;
+    color: white;
+    border-radius: 15px;
+    width: 30%;
+    text-align: center;
+    margin-top: 15%;
+  }
+</style>
