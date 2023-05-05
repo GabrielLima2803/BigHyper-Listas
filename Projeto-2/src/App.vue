3775fd5c845a7f3cffe71404302d9625594de060
GabrielLima2803 <gabriellima2803@gmail.com>
2023-05-05 16:55:20 -0300

    Fazer css carrinho || E grid

--- Projeto-2/src/App.vue
+++ Projeto-2/src/App.vue
@@ -4,84 +4,99 @@ const produtos = ref([
     {
         id: 1,
         nome: 'Camiseta',
+        quantidade: 0,
         preco: 49.90
     },
     {
         id: 2,
         nome: 'Calça',
+        quantidade: 0,
         preco: 99.90
-    },
-    {
-        id: 3,
-        nome: 'Meia',
-        preco: 9.90
-    },
-    {
-        id: 4,
-        nome: 'Sapato',
-        preco: 199.90
-    },
-    {
-        id: 5,
-        nome: 'Boné',
-        preco: 29.90
-    },
-    {
-        id: 6,
-        nome: 'Óculos',
-        preco: 99.90
-    },
-    {
-        id: 7,
-        nome: 'Relógio',
-        preco: 299.90
-    },
-    {
-        id: 8,
-        nome: 'Bermuda',
-        preco: 79.90
-    },
-    {
-        id: 9,
-        nome: 'Cueca',
-        preco: 19.90
-    },
-    {
-        id: 10,
-        nome: 'Meia',
-        preco: 9.90
-    }
-]
-)
-
-// Carrinho
-
-const carrinho = ref([
-    {
-        id: 1,
-        nome: 'Camiseta',
-        preco: 49.90,
-        quantidade: 1,
-        valorTotal: 49.90
-    },
-    {
-        id: 2,
-        nome: 'Calça',
-        preco: 99.90,
-        quantidade: 2,
-        valorTotal: 199.80
     },
     {
         id: 3,
         nome: 'Meia',
         preco: 9.90,
-        quantidade: 4,
-        valorTotal: 39.60
+        quantidade: 0
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        quantidade: 0,
+        preco: 199.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90,
+        quantidade: 0
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        quantidade: 0,
+        preco: 99.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        quantidade: 0,
+        preco: 299.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        quantidade: 0,
+        preco: 79.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        quantidade: 0,
+        preco: 19.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90,
+        quantidade: 0
     }
-    // total: 288.30
 ]
 )
 
+// Carrinho
+const carrinho = ref({
+  items: [],
+  total: 0
+})
+// Valor total 
+let valorTotal = ref(0)
+// Função adicionar carrinho
+function adicionarCarrinho(produto) {
+  carrinho.value.items.push({
+    id: produto.id, 
+    nome: produto.nome, 
+    preco: produto.preco, 
+    quantidade: produto.quantidade,
+    total: produto.preco * produto.quantidade
+  });
+  carrinho.value.total += produto.preco * produto.quantidade
+}
+// Função adicionar item
+function mais(index){
+  produtos.value[index].quantidade++
+  const pos = carrinho.value.items.indexOf(carrinho.value.items.find(c => c.id===produtos.value[index].id))
+  if(pos != -1) {
+    carrinho.value.total -= carrinho.value.items[pos].total
+    carrinho.value.items[pos].total = ++carrinho.value.items[pos].quantidade * carrinho.value.items[pos].preco 
+    carrinho.value.total += carrinho.value.items[pos].total
+ 
+  }
+}
+
+function menos(index){
+  produtos.value[index].quantidade--
+}
 
 </script>
 
@@ -90,21 +105,23 @@ const carrinho = ref([
 
   <!-- div que tem o v-for -->
  
-  <div v-for="(produto, id) in produtos" :key="id">{{ id }}: {{ produto.nome }}>
+<div v-for="(produto, index) in produtos" :key="produto.id"> 
+ 
+  <div  class="tema col-3"> 
+      <b> {{ produto.id }} - {{ produto.nome }} </b>
+  <br>
+  <h6>Preço: {{ produto.preco }}</h6>
+  <h6>Quantidade: {{ produto.quantidade }}</h6>
 
+    <button type="button" @click="menos(index)" class="btn bg-white p-1 h-r">-</button>
+    <button type="button" @click="mais(index)" class="btn bg-white p-1 ml-b h-g">+</button>
+    <button type="button" @click="adicionarCarrinho(produto)" class="btn bg-white p-1 ml-b h-g">Adicionar</button>
   </div>
 
-
-  <div class="tema col-3">
-    <p v-for="(produto, id) in produtos" :key="id">{{ id }}: {{ produto.nome }}</p>
-    <p v-for="(produto, id) in produtos" :key="id">{{  }} Preço do produto: {{ produto.preco }}</p>
-    <button type="button" class="btn bg-white p-1 h-r">-</button>
-    <button type="button" class="btn bg-white p-1 ml-b h-g">+</button>
-    <button type="button" class="btn bg-white p-1 ml-b h-g">Adicionar</button>
-  </div>
-  
-
-  
+<!-- FIM DIV FOR-V -->
+</div>
+<!-- FIM DIV.CONTAINER -->
+{{  carrinho }}
 </div>
 </template>
 
@@ -121,11 +138,10 @@ const carrinho = ref([
 } */
 .tema{
     background-color: black;
-    padding: 30px 30px;
+    padding: 25px;
     color: white;
     border-radius: 15px;
     width: 30%;
-    text-align: center;
     margin-top: 30px;
   }
   .ml-b{
